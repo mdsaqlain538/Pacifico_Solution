eb7e3e88642a0c5931b9755215c0ccd11d186b5c
mdsalman991 <43186964+mdsalman991@users.noreply.github.com>
2020-12-07 17:23:45 +0530

    Report Updated
    Update Report

---
+++ Report.js
@@ -0,0 +1,15 @@
+//Reports Regardig My Work
+
+
+
+//Date -> Dec -7 -2020
+//Server Creation Done.
+//server is hosted on local host 1519
+
+//Backend Operations.
+
+
+//SignUp functionality done.
+//SignIn module also done.
+
+//Both the above methods have be cross checked by using postman.
